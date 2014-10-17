0080f81ee471972876be9fe847f60da2c6f37fc0
Jaakko Ojalehto <jaakko.ojalehto@yle.fi>
2014-10-17 16:10:37 +0300

    Fix port to be compatible with heroku

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ var app = express();
 
 app.get('/', index.get);
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
